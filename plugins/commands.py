1a688bde38a3e6be1796e09fc16336c437be1de6
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-09-13 21:42:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,7 +12,17 @@ from Tools.Download import download
 
 
 my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
-support = "https://telegram.dog/Ns_Bot_supporters"
+support = "https://telegram.dog/Mo_Tech_YT"
+
+BUTTON = InlineKeyboardMarkup([[
+         InlineKeyboardButton("My Father 👨‍💻", url=my_father),
+         InlineKeyboardButton("📌Support channel", url=support)
+         ],[
+         InlineKeyboardButton("🖥️ How To Own This Bot 🖥️", url=support)
+         ]]
+         )
+         )
+
 
 @Client.on_message(Filters.command(["start"]))
 async def start(c, m):
@@ -20,7 +30,7 @@ async def start(c, m):
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.START.format(m.from_user.first_name, Config.USER_NAME),
                          reply_to_message_id=m.message_id,
-                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("My Father 👨‍💻", url=my_father), InlineKeyboardButton("📌Support channel", url=support)]]))
+                         reply_markup=BUTTON
     logger.info(f"{m.from_user.first_name} used start command")
 
 
