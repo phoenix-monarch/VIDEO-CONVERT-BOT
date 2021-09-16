403681b9a58026009005a6ff5b4cb32a6bd7faef
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-09-16 18:32:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -26,7 +26,6 @@ BUTTON = InlineKeyboardMarkup([[
          ]]
          )
 
-
 @Client.on_message(Filters.command(["start"]))
 async def start(c, m):
 
@@ -36,6 +35,38 @@ async def start(c, m):
                          reply_markup=BUTTON)
     logger.info(f"{m.from_user.first_name} used start command")
 
+@Client.on_message(Filters.private & Filters.command("password"))
+async def password(motech, update):
+    update_channel = UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await motech.get_chat_member(update_channel, update.chat.id)
+            if user.status == "kicked out":
+               await update.reply_text("😔 Sorry Dude, You are **🅱︎🅰︎🅽︎🅽︎🅴︎🅳︎ 😜**")
+               return
+        except UserNotParticipant:
+            #await update.reply_text(f"Join @{Channel User Name} To Use Me") From Motech.py
+            await update.reply_text(
+                text="First Join My Update Channel Then Will Get Password",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text="🔔 Join My Update Channel 🔔", url=f"t.me/{UPDATE_CHANNEL}")],
+                    ])
+            )
+            return
+        except Exception:
+            await update.reply_text(f"@{UPDATE_CHANNEL}")
+            return  
+    reply_markup =  PASS_BUTTON
+    await update.reply_text(
+        text=Translation.PASSWORD.format(update.from_user.mention, PASS_TEXT),
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+  )
+
+PASS_BUTTON = InlineKeyboardMarkup([[
+         InlineKeyboardButton("How To Own", url=f"{LINK}")
+         ]]
+         )
 
 
 @Client.on_message(Filters.command(["help"]))
