17c05563a143e86e2ea40298f5719a7c80693dd2
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-09-16 17:31:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,6 +6,7 @@ logger = logging.getLogger(__name__)
 import pyrogram
 from config import Config 
 from pyrogram import Client, Filters, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserNotParticipant
 from translation import Translation
 from Tools.Download import download
 
@@ -41,6 +42,35 @@ async def help(c, m):
                          text=Translation.HELP,
                          reply_to_message_id=m.message_id,
                          parse_mode="markdown")
+
+
+@Client.on_message(filters.private & filters.command("password"))
+async def password(motech, update):
+    update_channel = UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await motech.get_chat_member(update_channel, update.chat.id)
+            if user.status == "kicked out":
+               await update.reply_text("😔 Sorry Dude, You are **🅱︎🅰︎🅽︎🅽︎🅴︎🅳︎ 😜**")
+               return
+        except UserNotParticipant:
+            #await update.reply_text(f"Join @{Channel User Name} To Use Me") From Motech.py
+            await update.reply_text(
+                text="First Join My Update Channel Then Will Get Password",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text="🔔 Join My Update Channel 🔔", url=f"t.me/{UPDATE_CHANNEL}")],
+                    ])
+            )
+            return
+        except Exception:
+            await update.reply_text(f"@{UPDATE_CHANNEL}")
+            return  
+    reply_markup =  START_BUTTON
+    await update.reply_text(
+        text=Translation.PASSWORD.format(update.from_user.mention, PASS_TEXT),
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+  )
 
 
 @Client.on_message(Filters.command(["about"]))
